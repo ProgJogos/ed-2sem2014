6d7cb7f780b44a872105ab9f06bae64c3b0775b5
Enric Llagostera <enricllagostera@gmail.com>
2014-10-23 19:57:17 -0200

    gitignore e exemplo de componentes

---
+++ A03_Componentes/Assets/ControleJogador.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Movimento))]
+public class ControleJogador : MonoBehaviour 
+{
+	void Update () 
+	{
+		Vector2 direcaoInput = new Vector2 (
+			Input.GetAxis ("Horizontal"),
+			Input.GetAxis ("Vertical"));
+		this.GetComponent<Movimento>().direcaoAlvo = direcaoInput;
+	}
+}
---
+++ A03_Componentes/Assets/Movimento.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movimento : MonoBehaviour 
+{
+	public Vector2 direcaoAlvo;
+	public float aceleracao;
+	public float velocidadeMaxima;
+	
+	void FixedUpdate () 
+	{
+		if (direcaoAlvo.magnitude > 0.05f) 
+		{
+			transform.right = direcaoAlvo;
+		}
+		if (rigidbody2D.velocity.magnitude < velocidadeMaxima) 
+		{
+			rigidbody2D.AddForce(
+				transform.right * 
+				aceleracao *
+				direcaoAlvo.magnitude);
+		}
+		Debug.DrawRay (transform.position, 
+		              transform.right, 
+		              Color.red);
+	}
+}
