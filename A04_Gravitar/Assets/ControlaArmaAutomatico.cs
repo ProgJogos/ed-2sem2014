b497af82dcb6fb236e852d09a42eb63ee7eb88c5
Enric Llagostera <enricllagostera@gmail.com>
2014-10-30 13:46:54 -0200

    projeto gravitar

---
+++ A04_Gravitar/Assets/ControlaArmaAutomatico.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlaArmaAutomatico : MonoBehaviour 
+{
+	public Transform alvo;
+	public float frequencia;
+	public float margemErro;
+	private float timer = 0;
+	
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (timer > frequencia)
+		{
+			timer = 0;
+			Vector3 direcaoTiro = alvo.position - transform.position;
+			direcaoTiro.x = Random.Range(-margemErro/2, margemErro/2);
+			transform.GetComponent<Arma>().Disparar(direcaoTiro);
+		}
+	}
+}
