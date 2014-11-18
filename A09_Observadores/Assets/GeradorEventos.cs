460676685e889b3085693f32b82634224c869f13
Enric Llagostera <enricllagostera@gmail.com>
2014-11-18 06:11:46 -0200

    exemplo observadores

---
+++ A09_Observadores/Assets/GeradorEventos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate	void TratarEvento<T> (T dados);
+
+public class GeradorEventos : MonoBehaviour
+{
+	public static event TratarEvento<Transform> clicou;
+	
+	void OnMouseDown ()
+	{
+		if (clicou != null) {
+			clicou (transform);
+		}
+	}
+}
---
+++ A09_Observadores/Assets/TrocarCor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrocarCor : MonoBehaviour
+{
+	void Start ()
+	{
+		GeradorEventos.clicou += Trocar;
+	}
+	
+	void OnDestroy ()
+	{
+		GeradorEventos.clicou -= Trocar;
+	}
+	
+	void Trocar (Transform obj)
+	{
+		Color cor = new Color (Random.value, Random.value, Random.value);
+		//Camera.main.backgroundColor = cor;
+		obj.renderer.material.color = cor;
+		Debug.Log ("Trocou cor.");
+	}
+}
