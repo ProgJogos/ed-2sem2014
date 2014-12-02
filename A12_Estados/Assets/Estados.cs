a4345a9b37aaa2d8e9ca78800f07ff35ef0ee0ef
Enric Llagostera <enricllagostera@gmail.com>
2014-12-02 16:58:18 -0200

    entrada e saida de estados

--- A12_Estados/Assets/Estados.cs
+++ A12_Estados/Assets/Estados.cs
@@ -5,15 +5,16 @@ public interface IEstado
 {
 	void Update();
 	IEstado TratarInputs();
+	void Entrar();
+	void Sair();
 }
 
 [System.Serializable]
 public class Parado : IEstado
 {
-	public void Update()
-	{
-		
-	}
+	public void Update(){}
+	public void Entrar() {}
+	public void Sair() {}
 	
 	public IEstado TratarInputs()
 	{
@@ -40,7 +41,10 @@ public class Atirar : IEstado
 	public float velBala = 100f;
 	public float frequencia = 0.5f;
 	private float timer = 1f;
-	
+
+	public void Entrar() {}
+	public void Sair() {}
+
 	public void Update()
 	{
 		timer += Time.deltaTime;
@@ -70,24 +74,66 @@ public class Atirar : IEstado
 
 public class Andar : IEstado
 {
+
 	public float velocidade = 5f;
+
+	public void Entrar() {
+		this.velocidade = 5f;
+	}
+	public void Sair() {}
 
 	public void Update()
 	{
 		Vector2 direcao = new Vector2(
 			Input.GetAxis("Horizontal"),
 			Input.GetAxis("Vertical"));
+		if(direcao.magnitude > 0)
+			GameObject.Find("Jogador").transform.right = direcao;
 		GameObject.Find("Jogador").
-			transform.Translate(direcao * velocidade * Time.deltaTime);
+			transform.Translate(
+				direcao * velocidade * Time.deltaTime, Space.World);
 	}
 	
-	public IEstado TratarInputs()
+	public virtual IEstado TratarInputs()
 	{
 		if (Input.GetAxis("Horizontal") == 0 &&
 			Input.GetAxis("Vertical") == 0)
 		{
 			return new Parado();
 		}
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			return new Correr();
+		}
+
+		return this;
+	}
+}
+
+public class Correr : Andar 
+{
+
+	public void Entrar() {
+		this.velocidade = 15f;
+	}
+
+	public void Sair() {}
+
+	override public IEstado TratarInputs() 
+	{
+		if (Input.GetAxis("Horizontal") == 0 &&
+		    Input.GetAxis("Vertical") == 0 && 
+		    !Input.GetKey(KeyCode.C))
+		{
+			return new Parado();
+		}
+
+
+		if (Input.GetKeyUp(KeyCode.C))
+		{
+			return new Andar();
+		}
+
 		return this;
 	}
 }
--- A12_Estados/Assets/TesteEstados.cs
+++ A12_Estados/Assets/TesteEstados.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TesteEstados : MonoBehaviour
 {
 	public IEstado estadoAtual;
+	public IEstado estadoNovo;
 
 	void Start()
 	{
@@ -13,6 +14,12 @@ public class TesteEstados : MonoBehaviour
 	void Update()
 	{
 		estadoAtual.Update();
-		estadoAtual = estadoAtual.TratarInputs();
+		estadoNovo = estadoAtual.TratarInputs();
+		if (estadoNovo != estadoAtual)
+		{
+			estadoAtual.Sair();
+			estadoAtual = estadoNovo;
+			estadoAtual.Entrar();
+		}
 	}
 }
