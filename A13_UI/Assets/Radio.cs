baf0b9e5e3f91d41b7c31a97ffa969007032ab50
Enric Llagostera <enricllagostera@gmail.com>
2015-02-02 23:18:52 -0200

    exemplo gui

---
+++ A13_UI/Assets/Radio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radio : MonoBehaviour {
+
+	public void Tocar(AudioClip musica) {
+		audio.Stop ();
+		audio.clip = musica;
+		audio.Play();
+	}
+
+	public void MudarVolume (float valor) {
+		audio.volume = valor;
+	}
+
+	public void TocarTecla(AudioClip som) {
+		audio.Stop ();
+		audio.clip = som;
+		audio.volume = 0.2f;
+		audio.Play();
+	}
+}
